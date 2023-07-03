c63894c1a513a1e6b3f441befbcfa3cec9d98b07
xingmie <895170741@qq.com>
2023-07-03 10:58:26 +0800

    Initial commit

---
+++ src/main/java/name/modid/tremorModClient.java
@@ -0,0 +1,11 @@
+package name.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class tremorModClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
